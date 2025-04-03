879d19c9497ccd3e59c9b2a0f5a97409e214d7a0
madsfjeldberg <mafj0001@stud.kea.dk>
2025-04-03 10:06:58 +0200

    added card comp

---
+++ ui/components/Quiz.jsx
@@ -0,0 +1,46 @@
+import questions from "../../questions.json";
+import { Card } from "@rneui/themed";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { useState } from "react";
+export default function QuizCard({ question, options }) {
+  const allQuestions = questions.questions.flatMap((topic) => topic.questions);
+  const [questionList, setQuestionList] = useState(allQuestions);
+  const [currentQuestion, setCurrentQuestion] = useState(questionList[0]);
+
+  return (
+<Card containerStyle={styles.card}>
+        <Card.Title>{currentQuestion.question}</Card.Title>
+        <Card.Divider />
+        <TouchableOpacity style={styles.button}>
+          <Text>{currentQuestion.options[0]}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>{currentQuestion.options[1]}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>{currentQuestion.options[2]}</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.button}>
+          <Text>{currentQuestion.options[3]}</Text>
+        </TouchableOpacity>
+      </Card>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    width: "100%",
+    marginTop: 80,
+    margin: 0,
+    padding: 20,
+    borderRadius: 0,
+  },
+  button: {
+    backgroundColor: "lightgrey",
+    color: "white",
+    padding: 10,
+    borderRadius: 5,
+    margin: 10,
+  },
+});
