aba82ed64a3191b46c0cd310a85a25131548353a
madsfjeldberg <mafj0001@stud.kea.dk>
2025-04-03 10:05:07 +0200

    added card and content

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import questions from "./questions.json";
+import QuizCard from "./ui/components/Quiz";
 
 export default function App() {
+  const allQuestions = questions.questions.flatMap((topic) => topic.questions);
+  const [questionList, setQuestionList] = useState(allQuestions);
+  const [currentQuestion, setCurrentQuestion] = useState(questionList[0]);
+
   return (
     <View style={styles.container}>
-      <Text>Train your React Native Skills</Text>
+      <QuizCard question={currentQuestion.question} options={currentQuestion.options} />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +20,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
